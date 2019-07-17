0cc6370bda63b2b45ca8f3d8abe36b37ad9893e5
ssanchez <ssanchez@moval.es>
2019-07-17 14:20:09 +0200

    a

--- base_wua_infrastructure/models/wua_parcel.py
+++ base_wua_infrastructure/models/wua_parcel.py
@@ -315,7 +315,6 @@ class WuaParcel(models.Model):
                     UPDATE public.wua_parcel
                     SET area_gis = 0, with_gis_parcel = FALSE
                     """)
-                _logger = logging.getLogger(self.__class__.__name__)
                 for gis_parcel in gis_parcels:
                     name = gis_parcel[0]
                     geom = gis_parcel[1]
@@ -327,9 +326,10 @@ class WuaParcel(models.Model):
                         parcels.filtered(lambda x: x.name == name)
                     if len(filtered_parcels) == 1:
                         parcel = filtered_parcels[0]
+                        _logger = logging.getLogger(self.__class__.__name__)
                         _logger.info(area_gis)
                         parcel.area_gis = area_gis
-                
+                _logger = logging.getLogger(self.__class__.__name__)
                 _logger.info('Matching GIS info...')
                 _logger.info('Number of Odoo-Parcels: ' +
                              str(number_of_parcels))
