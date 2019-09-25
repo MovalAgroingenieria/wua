c2d73cf42caeef38cd93123260b2aeb75f0e1d4d
Salvador Sánchez Aroca <ssanchez@moval.es>
2019-09-25 14:02:01 +0200

    Added new menu on Account>sales, also created a tree and search view

--- base_wua_invoicing/__manifest__.py
+++ base_wua_invoicing/__manifest__.py
@@ -40,6 +40,7 @@
         "views/wua_irrigationditch_view.xml",
         "views/wua_irrigationshed_view.xml",
         "views/wua_hydraulicsector_view.xml",
+        "views/wua_invoiceline_view.xml",
         "views/res_partner_view.xml",
         "views/account_view.xml",
     ],
