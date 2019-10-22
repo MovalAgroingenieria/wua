9d4c2228ad9197291d4b76de377bad000ca0d998
Salvador Sánchez Aroca <ssanchez@moval.es>
2019-10-22 17:58:21 +0200

    2.3.4: Added to account_invoice_line the new field enrolledsubparcel_id and the comodel invoicelines_ids on wua_enrolledsubparcel.
    Added field sum_price_total on wua_enrolledsubparcel as the sum of all 
    price  subtotal's of invoiceliles associated with the enrolledsubparcel.
    
    Added new field number_of_invoicing_processes to wua_enrolledsubparcel 
    to know how many DISTINCT'S invoicests id's are associated with the 
    enrolledsubparcel.
    
    Added new field invoiced to know if the enrolledsubparcel at least has 
    one invoice associated

--- base_wua_invoicing_crop_planning/models/wua_cropplan.py
+++ base_wua_invoicing_crop_planning/models/wua_cropplan.py
@@ -37,6 +37,32 @@ class WuaEnrolledsubparcel(models.Model):
         compute='_compute_contracted_volume'
     )
 
+    invoiceline_ids = fields.One2many(
+        string='Invoice Lines',
+        comodel_name='account.invoice.line',
+        inverse_name='enrolledsubparcel_id'
+    )
+
+    sum_price_subtotal = fields.Float(
+        string='Amount',
+        store=True,
+        index=True,
+        compute='_compute_sum_price_subtotal'
+    )
+
+    number_of_invoicing_processes = fields.Float(
+        string='Number of invoicing processes',
+        store=True,
+        index=True,
+        compute='_compute_number_of_invoicing_processes'
+    )
+
+    invoiced = fields.Boolean(
+        string='Invoiced',
+        store=True,
+        compute='_compute_invoiced'
+    )
+
     @api.depends('area_official', 'agriculturalseason_id.volume_perunitarea')
     def _compute_contracted_volume(self):
         for record in self:
@@ -45,3 +71,30 @@ class WuaEnrolledsubparcel(models.Model):
                 contracted_volume = record.area_official * \
                     record.agriculturalseason_id.volume_perunitarea
             record.contracted_volume = contracted_volume
+
+    @api.depends('invoiceline_ids',
+                 'invoiceline_ids.price_subtotal')
+    def _compute_sum_price_subtotal(self):
+        for record in self:
+            sum_price_subtotal = 0
+            if (record.invoiceline_ids):
+                for invoiceline in record.invoiceline_ids:
+                    sum_price_subtotal += invoiceline.price_subtotal
+            record.sum_price_subtotal = sum_price_subtotal
+
+    @api.depends('invoiceline_ids')
+    def _compute_number_of_invoicing_processes(self):
+        for record in self:
+            number_of_invoicing_processes = 0
+            invoiceset_ids = []
+            for invoiceline in record.invoiceline_ids:
+                if (invoiceline.invoiceset_id not in invoiceset_ids):
+                    number_of_invoicing_processes += 1
+                    invoiceset_ids.appends(invoiceline.invoiceset_id)
+            record.number_of_invoicing_processes = \
+                number_of_invoicing_processes
+
+    @api.depends('number_of_invoicing_processes')
+    def _compute_invoiced(self):
+        for record in self:
+            record.invoiced = record.number_of_invoicing_processes > 0
