3f6caebcb54ddcf562abecd92140e9306de6e9b5
Salvador Sánchez Aroca <ssanchez@moval.es>
2019-12-13 11:22:35 +0100

    wua_crtorreabraham: Module creation: Created new custom module to delete product and taxes of the report account_invoice

---
+++ custom/crtorreabraham/wua_crtorreabraham/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Water Users Association Infrastructure (C.R.Torre Abraham)",
+    "summary": "Moval-Regadío customization for C.R.Torre.Abraham",
+    "version": '10.0.1.1.0',
+    "category": "Water Users Associations",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "account_wua", ],
+    "data": [
+        "reports/report_invoice_templates.xml", ],
+    "installable": True,
+    "application": False,
+}
