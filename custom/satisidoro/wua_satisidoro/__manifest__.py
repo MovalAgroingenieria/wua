a5c380290604da72c1943f2da553bf9f573c5bd2
Salvador Sánchez Aroca <ssanchez@moval.es>
2020-02-04 17:14:03 +0100

    [UPD] wua_satisidoro: Module creation.
    This module is in charge of modifying the wua_partner_report_document 
    adding to the partner info the number of shares and deleting the number 
    of votes, also, on the parcels of the partner it adds the cadastral 
    polygon, the cadastral parcel and the area official net and deletes 
    water costs, costs, irrigation partner and profile

---
+++ custom/satisidoro/wua_satisidoro/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Water Users Association Infrastructure (SAT nº 557 Isidoro)",
+    "summary": "Moval-Regadío customization for SAT Isidoro",
+    "version": '10.0.1.1.0',
+    "category": "Water Users Associations",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "base_wua", ],
+    "data": [
+        "reports/wua_partner_report.xml", ],
+    "installable": True,
+    "application": False,
+}
