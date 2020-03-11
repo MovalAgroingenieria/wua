72c6b118798f72b1c7eb6246d427fea48fe7d45c
Salvador Sánchez Aroca <ssanchez@moval.es>
2020-03-11 13:14:16 +0100

    [UPD] wua_cgrabaran: Chnages on wua_parcel_partnerlink model and view.
    - Added to wua_parcel_parcel_partnerlink two calculated fields named 
    'area_gis' and 'company_id' based on the parcel associated.
    
    -Added to wua_parcel_partnerlink_view_tree the new area_gis field
    
    - Added to wua_parcel_partnerlink_view_search a new group_by filter to 
    group by company_id
    
    - Modified the res_partner action to view partnerlinks the context to 
    activate by default the group_by filter of the company_id

--- custom/cgrabaran/wua_cgrabaran/models/wua_parcel.py
+++ custom/cgrabaran/wua_cgrabaran/models/wua_parcel.py
@@ -2,7 +2,7 @@
 # 2020 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api,  exceptions, _
+from odoo import models, fields, api
 
 
 class WuaParcel(models.Model):
@@ -12,3 +12,34 @@ class WuaParcel(models.Model):
         string='Company',
         comodel_name='res.company',
         index=True)
+
+
+class WuaParcelPartnerlink(models.Model):
+    _inherit = 'wua.parcel.partnerlink'
+
+    area_gis = fields.Float(
+        string='GIS Area',
+        digits=(32, 4),
+        default=0,
+        store=True,
+        index=True,
+        compute='_compute_area_gis')
+
+    company_id = fields.Many2one(
+        string='Company',
+        comodel_name='res.company',
+        store=True,
+        index=True,
+        compute='_compute_company_id')
+
+    @api.depends('parcel_id', 'parcel_id.area_gis')
+    def _compute_area_gis(self):
+        for record in self:
+            record.area_gis = record.parcel_id.area_gis if \
+                record.parcel_id.area_gis else 0
+
+    @api.depends('parcel_id', 'parcel_id.company_id')
+    def _compute_company_id(self):
+        for record in self:
+            record.company_id = record.parcel_id.company_id if \
+                record.parcel_id.company_id else 0
