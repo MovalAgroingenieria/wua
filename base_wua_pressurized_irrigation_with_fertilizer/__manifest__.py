267c926a44232969d04285a3938be8aa889e9c5d
Salvador Sánchez Aroca <ssanchez@moval.es>
2020-03-16 11:35:36 +0100

    [NEW] base_wua_pressurized_irrigation_with_fertilizer: Module creation.
    * Created Skeleton of new module to management of Fertilizer for WUA's

---
+++ base_wua_pressurized_irrigation_with_fertilizer/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# 2020 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Pressurized Irrigation Management for Water Users Associations",
+    "summary": "In a water users association, management of the "
+               "pressurized irrigation",
+    "version": '10.0.1.1.0',
+    "category": "Water Users Associations",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "base_wua_pressurized_irrigation",
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "application": True,
+}
