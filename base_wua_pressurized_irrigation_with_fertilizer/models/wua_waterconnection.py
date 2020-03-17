4a70a6579b4262b4723fcf6fcd408554b51ec655
Salvador Sánchez Aroca <ssanchez@moval.es>
2020-03-17 09:50:23 +0100

    [UPD] base_wua_pressurized_irrigation_with_filter: Added buttons to link models with fertconsumption.
    * Added button to show all fertconsumptions related to waterconnection, 
    presconsumption and agriculturalseason.

--- base_wua_pressurized_irrigation_with_fertilizer/models/wua_waterconnection.py
+++ base_wua_pressurized_irrigation_with_fertilizer/models/wua_waterconnection.py
@@ -2,7 +2,7 @@
 # 2020 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import models, fields, _, api
 
 
 class WuaWaterconnection(models.Model):
@@ -12,3 +12,29 @@ class WuaWaterconnection(models.Model):
         string='Fertilizers',
         comodel_name='wua.fertconsumption',
         inverse_name='waterconnection_id')
+
+    @api.multi
+    def action_see_fertconsumptions(self):
+        self.ensure_one()
+        condition = [('waterconnection_id', '=', self.id)]
+        id_form_view = self.env.ref(
+            'base_wua_pressurized_irrigation_with_fertilizer.'
+            'wua_fertconsumption_view_form').id
+        id_tree_view = self.env.ref(
+            'base_wua_pressurized_irrigation_with_fertilizer.'
+            'wua_fertconsumption_view_tree').id
+        search_view = self.env.ref(
+            'base_wua_pressurized_irrigation_with_fertilizer.'
+            'wua_fertconsumption_view_search')
+        act_window = {
+            'type': 'ir.actions.act_window',
+            'name': _('Consumptions'),
+            'res_model': 'wua.fertconsumption',
+            'view_type': 'form',
+            'view_mode': 'tree',
+            'views': [(id_tree_view, 'tree'), (id_form_view, 'form')],
+            'search_view_id': (search_view.id, search_view.name),
+            'domain': condition,
+            'target': 'current',
+            }
+        return act_window
