52d3131ae701df1fb2617d9d2617af632267c4e5
Salvador Sánchez Aroca <ssanchez@moval.es>
2020-05-19 09:38:11 +0200

    [NEW] base_wua_irrigation_report_with_quota: Module creation.
     * Model wua_irrigationreport, added field html_quota_balance and added 
    field to form view.

---
+++ base_wua_irrigation_report_with_quota/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# 2020 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Water Users Association: New functionalities of quotas "
+            "for irrigation reports",
+    "summary": "New improvements to the user experience, such as get the "
+               "current balances in form view of irrigation reports.",
+    "version": '10.0.1.1.0',
+    "category": "Water Users Associations",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "base_wua_irrigation_report",
+        "base_wua_quota_management", ],
+    "data": [
+        "views/wua_irrigationreport_view.xml", ],
+    "installable": True,
+    "application": False,
+}
