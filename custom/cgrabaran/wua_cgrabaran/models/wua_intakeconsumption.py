a907484419d9faff5c63e33fdb238fd51a0bb6cd
Salvador Sánchez Aroca <ssanchez@moval.es>
2020-05-28 13:51:03 +0200

    [UPD] wua_cgrabaran: Added posibility to change intake_id on flowreadings.
     * Now you can change the intake_id of a flowreading, this will be 
    reflected on a new field 'is_toll' which will be setted to true if the 
    intake_id is changed.
     * The is_toll field will be forwarded to the intakeconsumption 
    associated (if exists)

--- custom/cgrabaran/wua_cgrabaran/models/wua_intakeconsumption.py
+++ custom/cgrabaran/wua_cgrabaran/models/wua_intakeconsumption.py
@@ -13,6 +13,12 @@ class WuaIntakeconsumption(models.Model):
         store=True,
         compute='_compute_validated')
 
+    is_toll = fields.Boolean(
+        string='Toll Reading',
+        index=True,
+        store=True,
+        compute='_compute_is_toll')
+
     @api.depends('flowreading_id', 'flowreading_id.validated')
     def _compute_validated(self):
         for record in self:
@@ -20,3 +26,11 @@ class WuaIntakeconsumption(models.Model):
             if record.flowreading_id.validated:
                 validated = True
             record.validated = validated
+
+    @api.depends('flowreading_id', 'flowreading_id.is_toll')
+    def _compute_is_toll(self):
+        for record in self:
+            is_toll = False
+            if record.flowreading_id:
+                is_toll = record.flowreading_id.is_toll
+            record.is_toll = is_toll
