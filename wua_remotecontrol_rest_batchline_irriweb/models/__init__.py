ba7271dfaeec0de954835f01f2dd53109aec56c7
Salvador Sánchez Aroca <ssanchez@moval.es>
2020-07-06 12:28:25 +0200

    [UPD] wua_remotecontrol_rest_batchline_irriweb: Added telecontrol to res_partner.
     * Implemented hooks to update, create and delete partners on 
    batchline_irriweb remotecontrols

--- wua_remotecontrol_rest_batchline_irriweb/models/__init__.py
+++ wua_remotecontrol_rest_batchline_irriweb/models/__init__.py
@@ -2,5 +2,6 @@
 # 2020 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from . import res_partner
 from . import wua_irrigation_config_settings
 from . import wua_waterconnection
