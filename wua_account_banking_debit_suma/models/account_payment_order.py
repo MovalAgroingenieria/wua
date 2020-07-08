7d0b3c69820f8140e88280e09dde35d921b445fb
Juanu Sandoval <jsandoval@moval.es>
2020-07-08 20:13:41 +0200

    [UPD] wua_account_banking_debit_suma: Add method generated2uploaded

--- wua_account_banking_debit_suma/models/account_payment_order.py
+++ wua_account_banking_debit_suma/models/account_payment_order.py
@@ -2,7 +2,7 @@
 # 2019 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, _
+from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
 
@@ -42,3 +42,23 @@ class AccountPaymentOrder(models.Model):
                 _("Fail, partner code not found for partner %s." %
                   (partner_id.name)))
         return fixed_number
+
+    @api.multi
+    def generated2uploaded(self):
+        res = super(AccountPaymentOrder, self).generated2uploaded()
+        for order in self:
+            if order.payment_mode_id.name == 'SUMA':
+                for bline in order.bank_line_ids:
+                    if bline.suma_sent:
+                        invoice = self.env['account.invoice'].search([
+                            ('number', '=', bline.communication)])
+                        move_lines = self.env['account.move.line'].search([
+                            ('invoice_id', '=', invoice.id)])
+                        if move_lines:
+                            for move_line in move_lines:
+                                move_line.suma_ref = bline.suma_ref
+                        invoice.write({
+                            'in_suma': True,
+                            'suma_ref': bline.suma_ref,
+                        })
+        return res
