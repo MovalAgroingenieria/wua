4a80b8acbae2062d7fe82df973052435b45292d2
Salvador Sánchez Aroca <ssanchez@moval.es>
2020-09-02 10:18:17 +0200

    [NEW] base_wua_pressurized_irrigation_monitoring: Module skeleton and some model/views.
     * Created the skeleton of the new module.
     * Added the models/views of the wua_controlreading, 
    wua_controlpresconsumption and wua_negative_controlreading.
     * Added access rules to models added.

---
+++ base_wua_pressurized_irrigation_monitoring/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# 2020 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Water Users Association: Module to monitoring pressurized "
+            "irrigation",
+    "summary": "In a water users association, added functionality to manage "
+               "and compare estimated irrigation needs with real ones",
+    "version": '10.0.1.1.0',
+    "category": "Water Users Associations",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "base_wua_remotecontrol_rest", ],
+    "data": [
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "views/resources.xml",
+        "views/base_wua_pressurized_irrigation_monitoring_menus.xml",
+        # "views/wua_monitoring_config_settings_view.xml",
+        # "views/wua_parcel_view.xml",
+        # "views/wua_controlperiod_view.xml",
+        "views/wua_controlreading_view.xml",
+        "views/wua_controlpresconsumption_view.xml",
+        "views/wua_negative_controlreading_view.xml",
+        # "views/wua_comparative_subparcel_presconsumption_view.xml",
+        # "views/wua_comparative_parcel_presconsumption_view.xml",
+        # "views/wua_comparative_partner_presconsumption_view.xml",
+        # "views/res_partner_view.xml",
+        # "views/wua_cultivation_view.xml",
+        # "views/wua_negative_controlreading_view.xml", ],
+        ],
+    "installable": True,
+    "application": False,
+}
---
+++ base_wua_pressurized_irrigation_monitoring/static/src/js/button_import_readings.js
@@ -0,0 +1,52 @@
+odoo.define('base_wua_remotecontrol_rest.button_import_readings', function (require) {
+"use strict";
+
+var ListView = require('web.ListView');
+var Model = require('web.DataModel');
+var core = require('web.core');
+
+ListView.include({
+    render_buttons: function() {
+        this._super.apply(this, arguments);
+        if (this.$buttons) {
+            var btn = this.$buttons.find('.button_import_readings');
+            btn.on('click', this.proxy('do_button_import_readings'));
+        }
+    },
+    do_button_import_readings: function() {
+        var _t = core._t;
+        var message = _t('Import readings?')
+        var confirmed = confirm(message);
+        if (confirmed) {
+            var python_function = new Model('wua.reading').
+                                  call("do_import_readings",[]);
+            python_function.done(function(result) {
+                var title_number_of_readings = _t('Number of readings');
+                var title_error_message = _t('WARNING');
+                var number_of_readings = result[1];
+                var number_of_negative_readings = result[4];
+                var suffix_negative_readings = _t('negative readings')
+                var error_message = result[2];
+                var result_message = title_number_of_readings + ': ' +
+                                     number_of_readings.toString();
+                if (number_of_negative_readings > 0) {
+                    result_message = result_message + '\n\n' +
+                                     title_error_message + ': ' +
+                                     number_of_negative_readings.toString() +
+                                     ' ' + suffix_negative_readings;
+                }
+                if (error_message != '') {
+                    result_message = result_message + '\n\n' + 
+                                     title_error_message + ': ' +
+                                     error_message;
+                }
+                window.alert(result_message);
+                if (number_of_readings > 0) {
+                    window.location.reload(false);
+                }
+            });
+        }
+    }
+});
+
+})
