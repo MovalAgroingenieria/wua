e782378373fb33651a05ae355241d3ab18ef1851
Salvador Sánchez Aroca <ssanchez@moval.es>
2020-10-06 17:47:43 +0200

    [NEW] wua_crlibrilla: Module creation.
     * Created module for replacing the watering_duration of the 
    irrigationreports and use hours instead of minutes.

---
+++ custom/crlibrilla/wua_crlibrilla/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Water Users Association (C.R.Librilla)",
+    "summary": "Moval-Regadío customization for C.R.Librilla",
+    "version": '10.0.1.1.0',
+    "category": "Water Users Associations",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "base_wua_gravity_irrigation",
+    ],
+    "data": [
+        "views/resources.xml",
+        "views/wua_wateringrequest_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
