76d4dc8c9a6d963d2155f9292ee5eb1444d6a156
Salvador Sánchez Aroca <ssanchez@moval.es>
2020-11-19 10:02:29 +0100

    [NEW] wua_cralhama: Custom module creation.
     * Hide buttons on res partner form view for portal users.

---
+++ custom/cralhama/wua_cralhama/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# 2020 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Water Users Association Infrastructure (C.R.Alhama)",
+    "summary": "Moval-Regadío customization for C.R.Alhama",
+    "version": '10.0.1.1.0',
+    "category": "Water Users Associations",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "mail_tracking",
+        "base_wua_gravity_irrigation",
+    ],
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
