aed01f0de13351867fac75e13bf7b63570f8bd53
Salvador Sánchez Aroca <ssanchez@moval.es>
2020-12-29 11:28:22 +0100

    [UPD] base_wua_pressurized_irrigation: Some changes.
     * CSS changes for average_consumption field.
    
     * Added to migration script the setting of field 
    number_of_presconsumptions_for_average.

--- base_wua_pressurized_irrigation/migrations/10.0.1.1.1/post-migration.py
+++ base_wua_pressurized_irrigation/migrations/10.0.1.1.1/post-migration.py
@@ -7,6 +7,10 @@ from odoo import api, SUPERUSER_ID
 
 def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
+    values = env['ir.values'].sudo()
+    values.set_default('wua.irrigation.configuration',
+                       'number_of_presconsumptions_for_average',
+                       3)
     all_watermeters = env['wua.watermeter'].search([])
     all_watermeters._compute_average_consumption()
     all_presconsumptions = env['wua.presconsumption'].search([])
