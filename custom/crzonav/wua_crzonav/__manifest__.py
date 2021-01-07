55890b0813e3d8effb3be9f352141c032c3673a8
Salvador Sánchez Aroca <ssanchez@moval.es>
2021-01-07 13:42:47 +0100

    [NEW] wua_crzonav: Created custom module.

---
+++ custom/crzonav/wua_crzonav/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Zona V WUA modification",
+    "summary": "Custom modifications for Zona V",
+    "version": '10.0.1.1.0',
+    "category": "Water Users Associations",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "base_wua",
+    ],
+    "data": [
+        "views/wua_parcel_view.xml",
+        "views/res_partner_view.xml",
+        "views/resources.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
