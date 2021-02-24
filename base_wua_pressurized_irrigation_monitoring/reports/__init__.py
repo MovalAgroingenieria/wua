d009cd6ca45fe4da8e8a45685e03b05a5c806363
Juanu Sandoval <jsandoval@moval.es>
2021-02-24 19:53:34 +0100

    [UPD] base_wua_pressurized_irrigation_monitoring: Partner monitoring report creation

--- base_wua_pressurized_irrigation_monitoring/reports/__init__.py
+++ base_wua_pressurized_irrigation_monitoring/reports/__init__.py
@@ -2,6 +2,4 @@
 # 2021 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import models
-from . import wizard
-from . import reports
+from . import partner_presconsumption_monitoring_report
