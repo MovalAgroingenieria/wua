a9f5094ee40df050db28f2ee2e0aa5002974845d
Juanu Sandoval <jsandoval@moval.es>
2021-03-05 11:34:09 +0100

    [UPD] base_wua_pressurized_irrigation_monitoring: Add negative percentages to monitoring report

--- base_wua_pressurized_irrigation_monitoring/reports/partner_presconsumption_monitoring_report.py
+++ base_wua_pressurized_irrigation_monitoring/reports/partner_presconsumption_monitoring_report.py
@@ -68,8 +68,13 @@ class WuaComparativePartnerPresconsumption(models.Model):
         else:
             deviation = real_consumption - estimated_consumption
             deviation_percentage = 100
-            deviation = abs(deviation)
+            is_negative = False
+            if deviation < 0:
+                deviation = abs(deviation)
+                is_negative = True
             if deviation > 0 and real_consumption > 0:
                 deviation_percentage = \
                     (deviation * 100) / real_consumption
+            if is_negative:
+                deviation_percentage = deviation_percentage * -1
         return deviation_percentage
