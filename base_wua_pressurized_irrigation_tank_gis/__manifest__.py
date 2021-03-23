88f26852198d0b90cd55412120998bb72aee1eb1
Salvador Sánchez Aroca <ssanchez@moval.es>
2021-03-23 15:49:55 +0100

    [NEW] base_wua_pressurized_irrigation_tank_gis: Added gis frame for wua_tank.

---
+++ base_wua_pressurized_irrigation_tank_gis/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# 2021 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Water Users Association: GIS Tank Info",
+    "summary": "Added GIS Info to tank elements",
+    "version": '10.0.1.1.0',
+    "category": "Water Users Associations",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "base_wua_pressurized_irrigation_tank",
+    ],
+    "data": [
+        "views/wua_tank_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ base_wua_pressurized_irrigation_tank_gis/models/wua_tank.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# 2021 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields, api
+
+
+class WuaTank(models.Model):
+    _inherit = 'wua.tank'
+
+    html_gisviewer_frame = fields.Text(
+        string='GIS Viewer',
+        compute='_compute_html_gisviewer_frame'
+        )
+
+    @api.multi
+    def _compute_html_gisviewer_frame(self):
+        for record in self:
+            if record.with_gis_tank and \
+                    record.gis_viewer_link != '':
+                url = record.gis_viewer_link + '&mode=min'
+                url = url.replace('http://', 'https://')
+                record.html_gisviewer_frame = \
+                    '<p style="text-align:center;margin-top:0px;' + \
+                    'margin-left:6px;margin-right:6px;">' + \
+                    '<iframe id="iframe_tank" scrolling="no" ' + \
+                    'marginheight="0" marginwidth="0" ' + \
+                    'src="' + url + '" ' + \
+                    'frameborder="0" height="260" width="98%" ' + \
+                    '></iframe></p>'
+            else:
+                record.html_gisviewer_frame = ''
