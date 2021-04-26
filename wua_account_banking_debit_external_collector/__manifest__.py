2deee98f1913edf4c9b4ec2a751695e45b185dd1
Juanu Sandoval <jsandoval@moval.es>
2021-04-26 17:11:21 +0200

    [NEW] wua_account_banking_debit_external_collector: Module creation

---
+++ wua_account_banking_debit_external_collector/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "WUA Account Banking Debit External Collector",
+    "summary": 'This module is the companion of module Account Banking Dedit '
+               'External Collector',
+    "version": '10.0.1.1.0',
+    "category": "Moval General Addons",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "base_wua_invoicing",
+        "account_banking_debit_external_collector",
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ wua_account_banking_debit_external_collector/models/account_payment_order.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# 2020 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, api
+
+
+class AccountPaymentOrder(models.Model):
+    _inherit = 'account.payment.order'
+
+    @api.multi
+    def generated2uploaded(self):
+        res = super(AccountPaymentOrder, self).generated2uploaded()
+        for order in self:
+            if order.payment_mode_id.name == 'ext_collector':
+                for bline in order.bank_line_ids:
+                    if bline.ext_collector_sent:
+                        for l in bline.payment_line_ids:
+                            if bline.name == l.bank_line_id.name:
+                                invoice = l.invoice_id
+                                invoice.write({
+                                    'in_external_collector': True,
+                                    'external_collector_ref':
+                                        bline.external_collector_ref,
+                                    })
+                                move_lines = \
+                                    self.env['account.move.line'].search([
+                                        ('invoice_id', '=', invoice.id)])
+                            if move_lines:
+                                for move_line in move_lines:
+                                    move_line.external_collector_ref = \
+                                        bline.external_collector_ref
+        return res
