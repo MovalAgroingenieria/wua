b0304e97c466aab8342ff1ec6fa6376dc5c3e2e1
Juanu Sandoval <jsandoval@moval.es>
2021-07-20 21:16:39 +0200

    [NEW] wua_attendance_leaves_report: Module creation

---
+++ wua_attendance_leaves_report/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "WUA Employee Attendances / Leaves Report",
+    "summary": "WUA format for employee attendance/leaves report",
+    "version": '10.0.1.1.0',
+    "category": "Moval General Addons",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "attendance_leaves_report",
+        "base_wua_report",
+    ],
+    "data": [
+        "report/template_employee_attendance_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
+
