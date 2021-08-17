a31f49256eab6c801a5490df47054206cfbacaae
Juanu Sandoval <jsandoval@moval.es>
2021-08-17 16:35:43 +0200

    [UPD] account_wua: add journal_id to SQL sentence in overdue report

--- account_wua/reports/account_overdue_report.py
+++ account_wua/reports/account_overdue_report.py
@@ -11,7 +11,10 @@ class ReportOverdue(models.AbstractModel):
     def _get_account_move_lines(self, partner_ids):
         user_company_id = self.env.user.company_id.id
         res = dict(map(lambda x:(x,[]), partner_ids))
-        self.env.cr.execute("SELECT m.name AS move_id, l.date, l.name, l.ref, l.date_maturity, l.partner_id, l.blocked, l.amount_currency, l.currency_id, "
+        self.env.cr.execute(
+            "SELECT m.name AS move_id, l.date, l.name, l.ref, "
+            "l.date_maturity, l.partner_id, l.blocked, l.amount_currency, "
+            "l.currency_id, l.journal_id, j.name AS journal_name,"
             "CASE WHEN at.type = 'receivable' "
                 "THEN SUM(l.debit) "
                 "ELSE SUM(l.credit * -1) "
@@ -27,12 +30,15 @@ class ReportOverdue(models.AbstractModel):
             "FROM account_move_line l "
             "JOIN account_account_type at ON (l.user_type_id = at.id) "
             "JOIN account_move m ON (l.move_id = m.id) "
-            "WHERE l.partner_id IN %s AND at.type IN ('receivable', 'payable') "
+            "JOIN account_journal j ON (l.journal_id = j.id) "
+            "WHERE l.partner_id IN %s AND at.type IN ('receivable','payable') "
             "AND l.full_reconcile_id IS NULL "
             "AND l.company_id = %s"
             "GROUP BY l.date, l.name, l.ref, l.date_maturity, l.partner_id, "
-            "at.type, l.blocked, l.amount_currency, l.currency_id, l.move_id, m.name",
-            (((fields.date.today(), ) + (tuple(partner_ids), user_company_id))))
+            "at.type, l.blocked, l.amount_currency, l.currency_id, l.move_id, "
+            "m.name, l.journal_id, j.name",
+            (((fields.date.today(), ) + (tuple(partner_ids),
+                                         user_company_id))))
         for row in self.env.cr.dictfetchall():
             res[row.pop('partner_id')].append(row)
         return res
