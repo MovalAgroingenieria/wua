da2022602b678c8469f93125a582de82bf149272
Salvador Sánchez Aroca <ssanchez@moval.es>
2021-09-06 14:07:25 +0200

    [UPD] base_wua_invoicing_pressurized_irrigation: Add constraints to avoid some changes on invoiced readings.
     * Cannot unlink invoiced readings.
     * If a reading is invoiced, cannot change values of volume and validated fields.

--- base_wua_invoicing_pressurized_irrigation/models/wua_reading.py
+++ base_wua_invoicing_pressurized_irrigation/models/wua_reading.py
@@ -2,7 +2,7 @@
 # 2020 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions, _
 
 
 class WuaReading(models.Model):
@@ -23,3 +23,24 @@ class WuaReading(models.Model):
                 invoiced_reading = \
                     record.presconsumption_id.invoiced_consumption
             record.invoiced_reading = invoiced_reading
+
+    @api.multi
+    def unlink(self):
+        for record in self:
+            if (record.invoiced_reading):
+                raise exceptions.UserError(
+                    _('Cannot delete reading with invoiced consumptions.'))
+        return super(WuaReading, self).unlink()
+
+    @api.multi
+    def write(self, vals):
+        readonly_values_when_invoiced = ['volume', 'validated']
+        for record in self:
+            # Check if any of trigger values appears and then check if is an
+            # already invoiced reading
+            if (any([i in vals for i in readonly_values_when_invoiced])):
+                if (record.invoiced_reading):
+                    raise exceptions.UserError(
+                        _('Cannot update reading with invoiced consumptions.'))
+        # Call inherited write operation.
+        return super(WuaReading, self).write(vals)
