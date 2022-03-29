625383dbd323db5368866b59938b89cbe900c0c4
Salvador Sanchez Aroca <ssanchez@moval.es>
2022-03-29 10:36:56 +0200

    [NEW] base_wua_crop_planning: Module creation.  * Module used to move Crop Plans menu entry from Pressurize Irrigation -> Own Menu on wua_structure_irrigation.

---
+++ base_wua_crop_planning_with_menu/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# 2022 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Water Users Association: Crop planning with own menu",
+    "summary": "Menu for crop plannings is now on it's own section inside "
+               "wua structure irrigation menu",
+    "version": '10.0.1.1.0',
+    "category": "Water Users Associations",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "base_wua_crop_planning", ],
+    "data": [
+        "views/wua_cropplan_view.xml", ],
+    "installable": True,
+    "application": False,
+}
