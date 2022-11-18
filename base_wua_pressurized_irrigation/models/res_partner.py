8fb2cf65817ded745d79b32233cec6e72e7a05ea
Salvador Sanchez Aroca <ssanchez@moval.es>
2022-11-18 11:32:16 +0100

    [UPD] base_wua_pressurized_irrigation: Added new field watermeter_id to res_partner_waterconnection model and views.

--- base_wua_pressurized_irrigation/models/res_partner.py
+++ base_wua_pressurized_irrigation/models/res_partner.py
@@ -2,7 +2,7 @@
 # 2019 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api, exceptions, _, tools
+from odoo import models, fields, api, tools
 
 
 class ResPartnerWaterconnection(models.Model):
@@ -16,6 +16,11 @@ class ResPartnerWaterconnection(models.Model):
         string='Last Reading Value',
         digits=(32, 4),
     )
+
+    watermeter_id = fields.Many2one(
+        string='Watermeter',
+        comodel_name='wua.watermeter',
+        related='waterconnection_id.watermeter_id',)
 
     @api.model_cr
     def init(self):
