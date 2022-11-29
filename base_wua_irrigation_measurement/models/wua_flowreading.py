dd8b7b2b03f8f378a00f55b947008dd55de71e11
Salvador Sanchez Aroca <ssanchez@moval.es>
2022-11-29 17:18:22 +0100

    [UPD] base_wua_irrigation_measurement: Fixed compare between dates (datetime being compared with string) .

--- base_wua_irrigation_measurement/models/wua_flowreading.py
+++ base_wua_irrigation_measurement/models/wua_flowreading.py
@@ -191,7 +191,7 @@ class WuaFlowreading(models.Model):
                 [('flowmeter_id', '=', vals['flowmeter_id'])],
                 limit=1, order='reading_time desc')
             if (last_reading and last_reading.reading_time >
-                    reading_end_time):
+                    fields.Datetime.from_string(reading_end_time)):
                 raise exceptions.UserError(_('The reading time is minor '
                                              'than the time of the previous '
                                              'reading.'))
