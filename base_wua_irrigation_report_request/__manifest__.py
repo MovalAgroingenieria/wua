57c03deeb6f90240e47db345ef5572df244bbf19
Juanu Sandoval <jsandoval@moval.es>
2022-11-29 13:32:59 +0100

    [UPD] base_wua_irrigation_report_request & base_wua_pressurized_irrigation: Change report_qr dependence

--- base_wua_irrigation_report_request/__manifest__.py
+++ base_wua_irrigation_report_request/__manifest__.py
@@ -14,7 +14,6 @@
     "license": "AGPL-3",
     "depends": [
         "base_wua_invoicing_irrigation_report",
-        "report_qr",
     ],
     "data": [
         "security/security.xml",
--- base_wua_pressurized_irrigation/__manifest__.py
+++ base_wua_pressurized_irrigation/__manifest__.py
@@ -13,6 +13,7 @@
     "license": "AGPL-3",
     "depends": [
         "wua_structure_irrigation",
+        "report_qr",
     ],
     "data": [
         "security/ir.model.access.csv",
