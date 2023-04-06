bc29c52c91fec1804e30d6b9694aab102aa67185
einiesta <einiesta@moval.es>
2023-04-06 12:25:18 +0200

    [UPD] base_wua_quota_management: warning for the changes in partnerlink if there is water costs

--- base_wua_quota_management/models/wua_parcel.py
+++ base_wua_quota_management/models/wua_parcel.py
@@ -2,7 +2,8 @@
 # 2020 - Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api, _
+from datetime import datetime
+from odoo import models, fields, api, exceptions, _
 
 
 class WuaParcel(models.Model):
@@ -22,6 +23,11 @@ class WuaParcel(models.Model):
         string='Current Quota Period',
         comodel_name='wua.quotaperiod',
         compute='_compute_current_quotaperiod_id')
+
+    selected_for_current_quotaperiod = fields.Boolean(
+        string='Parcel present in the current quota period',
+        default=False,
+        compute='_compute_selected_for_current_quotaperiod')
 
     @api.multi
     def _compute_current_quotaperiod_id(self):
@@ -78,3 +84,65 @@ class WuaParcel(models.Model):
             'target': 'new'
             }
         return act_window
+
+    @api.multi
+    def _compute_selected_for_current_quotaperiod(self):
+        for record in self:
+            selected_for_current_quotaperiod = False
+            self.partnerlink_ids.__class__._fired_onchange_partner = False
+            self.partnerlink_ids.__class__._fired_onchange_watercosts = False
+            current_date = datetime.today().strftime('%Y-%m-%d')
+            if record.id > 0:
+                self.env.cr.execute("""
+                    select count(*)
+                    from wua_quotaperiod_line_parcel qplp
+                    inner join wua_quotaperiod_line qpl
+                    on qplp.quotaperiodline_id = qpl.id
+                    inner join wua_quotaperiod qp on
+                    qpl.quotaperiod_id = qp.id
+                    where qplp.selected = true and qp.state='generated' and
+                    qp.initial_date <= %s and qp.end_date >= %s and
+                    qplp.parcel_id = %s""", (current_date,
+                                             current_date, record.id))
+                query_results = self.env.cr.dictfetchall()
+                if (query_results and query_results[0].get('count') is not None
+                   and query_results[0].get('count') > 0):
+                    selected_for_current_quotaperiod = True
+            record.selected_for_current_quotaperiod = \
+                selected_for_current_quotaperiod
+
+
+class WuaParcelPartnerlink(models.Model):
+    _inherit = 'wua.parcel.partnerlink'
+
+    _fired_onchange_partner = False
+    _fired_onchange_watercosts = False
+
+    @api.onchange('partner_id')
+    def _onchange_partner(self):
+        if (self.parcel_id.selected_for_current_quotaperiod and
+           self.partner_id and
+           self.water_costs_percentage and
+           (not self.__class__._fired_onchange_partner)):
+            self.__class__._fired_onchange_partner = True
+            warning_title = _('IMPORTANT WARNING:')
+            warning_text = _('A change has been detected in the partners '
+                             'that take charge of the water costs. Consider '
+                             'making any necessary adjustments to the '
+                             'distribution of the affected quotas, or click '
+                             'DISCARD to undo the change.')
+            raise exceptions.UserError(warning_title + '\n\n' + warning_text)
+
+    @api.onchange('water_costs_percentage')
+    def _onchange_watercosts(self):
+        if (self.parcel_id.selected_for_current_quotaperiod and
+           self.partner_id and
+           (not self.__class__._fired_onchange_watercosts)):
+            self.__class__._fired_onchange_watercosts = True
+            warning_title = _('IMPORTANT WARNING:')
+            warning_text = _('A change in the percentage of water costs of '
+                             'the parcel has been detected. Consider making '
+                             'the necessary adjustments to the distribution '
+                             'of the affected quotas, or click DISCARD to '
+                             'undo the change.')
+            raise exceptions.UserError(warning_title + '\n\n' + warning_text)
