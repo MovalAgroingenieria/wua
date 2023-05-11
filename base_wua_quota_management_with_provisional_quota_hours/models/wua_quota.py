d3d223b0ace136aa53d35fad1093f30dff8c05ef
Salvador Sanchez Aroca <ssanchez@moval.es>
2023-05-11 12:53:14 +0200

    [UPD] base_wua_quota_management_with_provisional_quota_hours: Add information about provisional balance and extra consumtpion in hours to aggregated quotas.

--- base_wua_quota_management_with_provisional_quota_hours/models/wua_quota.py
+++ base_wua_quota_management_with_provisional_quota_hours/models/wua_quota.py
@@ -2,8 +2,7 @@
 # 2021 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import datetime
-from odoo import models, fields, api, _
+from odoo import models, fields, api
 
 
 class WuaQuota(models.Model):
@@ -24,7 +23,6 @@ class WuaQuota(models.Model):
     provisional_balance_vol_hours = fields.Char(
         string='Balance provisional',
         compute='_compute_provisional_balance_vol_hours')
-
 
     @api.multi
     def _compute_provisional_extra_consumption_hours(self):
@@ -55,6 +53,62 @@ class WuaQuota(models.Model):
         for record in self:
             provisional_balance_vol_hours = \
                 self.transform_to_quota_hours_format_form_view(
+                    record.provisional_balance,
+                    record.provisional_balance_hours)
+            record.provisional_balance_vol_hours = \
+                provisional_balance_vol_hours
+
+
+class WuaQuotaAggregatevalue(models.Model):
+    _inherit = 'wua.quota.aggregatevalue'
+
+    provisional_extra_consumption_hours = fields.Char(
+        string='Extra consumptions (hours)',
+        compute='_compute_provisional_extra_consumption_hours')
+
+    provisional_extra_consumption_vol_hours = fields.Char(
+        string='Extra consumptions',
+        compute='_compute_provisional_extra_consumption_vol_hours')
+
+    provisional_balance_hours = fields.Char(
+        string='Balance provisional (hours)',
+        compute='_compute_provisional_balance_hours')
+
+    provisional_balance_vol_hours = fields.Char(
+        string='Balance provisional',
+        compute='_compute_provisional_balance_vol_hours')
+
+    @api.multi
+    def _compute_provisional_extra_consumption_hours(self):
+        for record in self:
+            record.provisional_extra_consumption_hours = \
+                self.env['wua.quota'].transform_to_quota_hours_format(
+                    record.provisional_extra_consumption)
+
+    @api.multi
+    def _compute_provisional_extra_consumption_vol_hours(self):
+        for record in self:
+            provisional_extra_consumption_vol_hours = \
+                self.env['wua.quota'].\
+                transform_to_quota_hours_format_form_view(
+                    record.provisional_extra_consumption,
+                    record.provisional_extra_consumption_hours)
+            record.provisional_extra_consumption_vol_hours = \
+                provisional_extra_consumption_vol_hours
+
+    @api.multi
+    def _compute_provisional_balance_hours(self):
+        for record in self:
+            record.provisional_balance_hours = \
+                self.env['wua.quota'].transform_to_quota_hours_format(
+                    record.provisional_balance)
+
+    @api.multi
+    def _compute_provisional_balance_vol_hours(self):
+        for record in self:
+            provisional_balance_vol_hours = \
+                self.env['wua.quota'].\
+                transform_to_quota_hours_format_form_view(
                     record.provisional_balance,
                     record.provisional_balance_hours)
             record.provisional_balance_vol_hours = \
