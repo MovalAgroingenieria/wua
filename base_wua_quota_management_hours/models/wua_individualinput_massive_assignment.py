8f4e0d82dc39d247f70d3055be1941ca6e7f3496
Salvador Sanchez Aroca <ssanchez@moval.es>
2023-07-10 08:25:13 +0200

    [UPD] base_wua_quota_management_hours: Flake8 fixes; Remvoing unused libraries.

--- base_wua_quota_management_hours/models/wua_individualinput_massive_assignment.py
+++ base_wua_quota_management_hours/models/wua_individualinput_massive_assignment.py
@@ -2,14 +2,7 @@
 # Copyright 2023 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import datetime
-import pytz
-import locale
-from odoo import models, fields, api, exceptions, _
-
-
-class WuaIndividualinputMassiveAssignment(models.Model):
-    _inherit = 'wua.individualinput.massive.assignment'
+from odoo import models, fields, api
 
 
 class WuaIndividualinputMassiveAssignmentLine(models.Model):
