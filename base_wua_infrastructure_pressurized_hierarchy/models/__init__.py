78e11f0713563008db177bd65befdba1fc522e46
Salvador Sanchez Aroca <ssanchez@moval.es>
2023-09-07 16:30:23 +0200

    [UPD] base_wua_infrastructure_pressurized_hierarchy: Added Many2one relation with airvalve, valve and drainagevalve with waterpipe.

--- base_wua_infrastructure_pressurized_hierarchy/models/__init__.py
+++ base_wua_infrastructure_pressurized_hierarchy/models/__init__.py
@@ -6,3 +6,6 @@ from . import wua_waterpipe
 from . import wua_hydraulicsector
 from . import wua_irrigationshed
 from . import wua_parcel
+from . import wua_airvalve
+from . import wua_drainagevalve
+from . import wua_valve
---
+++ base_wua_infrastructure_pressurized_hierarchy/models/wua_airvalve.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# 2023 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields, api
+
+
+class WuaAirvalve(models.Model):
+    _inherit = 'wua.airvalve'
+
+    waterpipe_id = fields.Many2one(
+        string='Waterpipe',
+        comodel_name='wua.waterpipe',
+        index=True,
+    )
+
+    @api.onchange('waterpipe_id')
+    def _onchange_waterpipe_id(self):
+        if (self.waterpipe_id):
+            self.hydraulicsector_id = self.waterpipe_id.hydraulicsector_id
+
+    @api.model
+    def create(self, vals):
+        if 'waterpipe_id' in vals:
+            if vals['waterpipe_id']:
+                wp = self.env['wua.waterpipe'].browse(vals['waterpipe_id'])
+                vals['hydraulicsector_id'] = wp.hydraulicsector_id.id
+        return super(WuaAirvalve, self).create(vals)
+
+    @api.multi
+    def write(self, vals):
+        if 'waterpipe_id' in vals:
+            if vals['waterpipe_id']:
+                wp = self.env['wua.waterpipe'].browse(vals['waterpipe_id'])
+                vals['hydraulicsector_id'] = wp.hydraulicsector_id.id
+        return super(WuaAirvalve, self).write(vals)
