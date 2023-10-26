380b0204e9541b8983a17f53c2fe627cc3caaa68
Juanu Sandoval <jsandoval@moval.es>
2023-10-26 17:39:13 +0200

    [UPD] base_wua_infrastructure_pressurized_hierarchy: Comment method fields_get in wua_parcel and wua_waterpipe due to errors (messages in chatter)

--- base_wua_infrastructure_pressurized_hierarchy/models/wua_parcel.py
+++ base_wua_infrastructure_pressurized_hierarchy/models/wua_parcel.py
@@ -301,18 +301,20 @@ class WuaParcel(models.Model):
         store=True,
         compute="_compute_waterpipe_40_id")
 
-    @api.model
-    def fields_get(self, fields=None, allfields=None, attributes=None):
-        fields_to_hide = []
-        for parcel_level in range(6, 41):
-            fields_to_hide.append(
-                'waterpipe_' + str(parcel_level).zfill(2) + '_id')
-        res = super(WuaParcel, self).fields_get(
-            allfields=None, attributes=None)
-        for field in fields_to_hide:
-            res[field]['selectable'] = False
-            res[field]['sortable'] = False
-        return res
+    # INFO: This method causes all fields with track_visibility to be added to
+    #       the chatter, even when another parameter with no tracking changes.
+    # @api.model
+    # def fields_get(self, fields=None, allfields=None, attributes=None):
+    #     fields_to_hide = []
+    #     for parcel_level in range(6, 41):
+    #         fields_to_hide.append(
+    #             'waterpipe_' + str(parcel_level).zfill(2) + '_id')
+    #     res = super(WuaParcel, self).fields_get(
+    #         allfields=None, attributes=None)
+    #     for field in fields_to_hide:
+    #         res[field]['selectable'] = False
+    #         res[field]['sortable'] = False
+    #     return res
 
     @api.depends('irrigationpoint_ids', 'irrigationpoint_ids.waterpipe_id')
     def _compute_waterpipe_id(self):
