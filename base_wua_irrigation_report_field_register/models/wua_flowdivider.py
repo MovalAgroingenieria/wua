cbdcd69b06f5ff13d535bb743b30807f12c67836
Salvador Sanchez Aroca <ssanchez@moval.es>
2023-11-03 13:43:29 +0100

    [UPD] base_wua_irrigation_report_field_register: Updated flowdivider view to avoid newline inconsistencies.

--- base_wua_irrigation_report_field_register/models/wua_flowdivider.py
+++ base_wua_irrigation_report_field_register/models/wua_flowdivider.py
@@ -2,7 +2,7 @@
 # 2022 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api, exceptions, _
+from odoo import models, fields
 
 
 class WuaFlowdivider(models.Model):
