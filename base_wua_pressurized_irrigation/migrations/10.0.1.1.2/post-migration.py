c5650c2e0a64a9ec255ea307e192e478d501b03a
Miguel Ángel Rodríguez <marodriguez@moval.es>
2024-03-04 10:03:20 +0100

    [FIX] base_wua_pressurized_irrigation: Fix the character limit and the migration script with the correct sql query

--- base_wua_pressurized_irrigation/migrations/10.0.1.1.2/post-migration.py
+++ base_wua_pressurized_irrigation/migrations/10.0.1.1.2/post-migration.py
@@ -11,12 +11,23 @@ def migrate(cr, version):
     env.cr.execute("""
         CREATE OR REPLACE VIEW res_partner_waterconnection AS (
         SELECT row_number() OVER() AS id, a.* FROM (
-            SELECT wpp1.partner_id, wpi1.waterconnection_id, wpi1.active
+            SELECT wpp1.partner_id, wpi1.waterconnection_id, 
+            wpi1.active,
+            ww1.last_reading_time, ww1.last_reading_value,
+            wpc1.volume_real
             FROM
-            wua_parcel_irrigationpoint wpi1 INNER JOIN
-            wua_waterconnection ww1 ON ww1.id = wpi1.waterconnection_id
-            INNER JOIN wua_parcel_partnerlink wpp1 ON wpp1.parcel_id =
-            wpi1.parcel_id WHERE wpi1.type='WC'
-            GROUP BY  wpp1.partner_id, wpi1.waterconnection_id, wpi1.active
+            wua_parcel_irrigationpoint wpi1
+            INNER JOIN  wua_waterconnection ww1
+            ON ww1.id = wpi1.waterconnection_id
+            INNER JOIN wua_parcel_partnerlink wpp1
+            ON wpp1.parcel_id = wpi1.parcel_id
+            LEFT JOIN wua_presconsumption wpc1
+            ON wpc1.waterconnection_id = ww1.id
+            AND wpc1.reading_end_time = ww1.last_reading_time
+            WHERE wpi1.type='WC' AND ww1.watermeter_id IS NOT NULL
+            GROUP BY  wpp1.partner_id, wpi1.waterconnection_id,
+             wpi1.active,
+            ww1.last_reading_time, ww1.last_reading_value,
+            wpc1.volume_real
         ) a )
         """)
--- base_wua_pressurized_irrigation/models/res_partner.py
+++ base_wua_pressurized_irrigation/models/res_partner.py
@@ -40,7 +40,8 @@ class ResPartnerWaterconnection(models.Model):
             self.env.cr.execute("""
                 CREATE OR REPLACE VIEW res_partner_waterconnection AS (
                 SELECT row_number() OVER() AS id, a.* FROM (
-                    SELECT wpp1.partner_id, wpi1.waterconnection_id, wpi1.active
+                    SELECT wpp1.partner_id, wpi1.waterconnection_id, 
+                    wpi1.active,
                     ww1.last_reading_time, ww1.last_reading_value,
                     wpc1.volume_real
                     FROM
@@ -53,7 +54,8 @@ class ResPartnerWaterconnection(models.Model):
                     ON wpc1.waterconnection_id = ww1.id
                     AND wpc1.reading_end_time = ww1.last_reading_time
                     WHERE wpi1.type='WC' AND ww1.watermeter_id IS NOT NULL
-                    GROUP BY  wpp1.partner_id, wpi1.waterconnection_id, wpi1.active,
+                    GROUP BY  wpp1.partner_id, wpi1.waterconnection_id,
+                     wpi1.active,
                     ww1.last_reading_time, ww1.last_reading_value,
                     wpc1.volume_real
                 ) a )
