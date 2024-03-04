b7f435a1b85e115d421fdb7a92b24531ce30249f
Miguel Ángel Rodríguez <marodriguez@moval.es>
2024-03-04 10:02:55 +0100

    [FIX] base_wua_infrastructure: Fix the character limit and the migration script with the correct sql query

--- base_wua_infrastructure/migrations/10.0.1.1.3/post-migration.py
+++ base_wua_infrastructure/migrations/10.0.1.1.3/post-migration.py
@@ -11,12 +11,14 @@ def migrate(cr, version):
     env.cr.execute("""
         CREATE OR REPLACE VIEW res_partner_waterconnection AS (
         SELECT row_number() OVER() AS id, a.* FROM (
-            SELECT wpp1.partner_id, wpi1.waterconnection_id, wpi1.active
+            SELECT wpp1.partner_id, wpi1.waterconnection_id, 
+            wpi1.active
             FROM
             wua_parcel_irrigationpoint wpi1 INNER JOIN
             wua_waterconnection ww1 ON ww1.id = wpi1.waterconnection_id
             INNER JOIN wua_parcel_partnerlink wpp1 ON wpp1.parcel_id =
             wpi1.parcel_id WHERE wpi1.type='WC'
-            GROUP BY  wpp1.partner_id, wpi1.waterconnection_id, wpi1.active
+            GROUP BY  wpp1.partner_id, wpi1.waterconnection_id, 
+            wpi1.active
         ) a )
         """)
