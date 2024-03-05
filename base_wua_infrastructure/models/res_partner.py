0397f998c8897360a14f5c6d274548000ff59934
Miguel Ángel Rodríguez <marodriguez@moval.es>
2024-03-05 17:16:43 +0100

    [FIX] base_wua_infrastructure: correct whitespace in line breaks and translations

--- base_wua_infrastructure/models/res_partner.py
+++ base_wua_infrastructure/models/res_partner.py
@@ -130,14 +130,14 @@ class ResPartnerWaterconnection(models.Model):
             self.env.cr.execute("""
                 CREATE OR REPLACE VIEW res_partner_waterconnection AS (
                 SELECT row_number() OVER() AS id, a.* FROM (
-                    SELECT wpp1.partner_id, wpi1.waterconnection_id, 
+                    SELECT wpp1.partner_id, wpi1.waterconnection_id,
                     wpi1.active
                     FROM
                     wua_parcel_irrigationpoint wpi1 INNER JOIN
                     wua_waterconnection ww1 ON ww1.id = wpi1.waterconnection_id
                     INNER JOIN wua_parcel_partnerlink wpp1 ON wpp1.parcel_id =
                     wpi1.parcel_id WHERE wpi1.type='WC'
-                    GROUP BY  wpp1.partner_id, wpi1.waterconnection_id, 
+                    GROUP BY  wpp1.partner_id, wpi1.waterconnection_id,
                     wpi1.active
                 ) a )
                 """)
