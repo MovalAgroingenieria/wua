c0315621e1a9bb53cd27cfb2b47836a1fc8880a4
Miguel Ángel Rodríguez <marodriguez@moval.es>
2024-03-05 12:15:09 +0100

    [UPD] base_wua_infrastructure_gravity_hierarchy: Add button pivot type view in parcels view accessing from irrigation ditch or drainage ditch.

--- base_wua_infrastructure_gravity_hierarchy/models/wua_drainageditch.py
+++ base_wua_infrastructure_gravity_hierarchy/models/wua_drainageditch.py
@@ -648,13 +648,17 @@ class WuaDrainageditch(models.Model):
             id_form_view = \
                 self.env.ref('base_wua.'
                              'wua_parcel_view_form').id
+            id_pivot_view = \
+                self.env.ref('base_wua.'
+                             'wua_parcel_view_pivot').id
             act_window = {
                 'type': 'ir.actions.act_window',
                 'name': _('Parcels'),
                 'res_model': 'wua.parcel',
                 'view_type': 'form',
                 'view_mode': 'tree',
-                'views': [(id_tree_view, 'tree'), (id_form_view, 'form')],
+                'views': [(id_tree_view, 'tree'), (id_form_view, 'form'),
+                          (id_pivot_view, 'pivot')],
                 'domain': condition,
                 'target': 'current',
                 'context': self.env.context,
