6ccca0f9d87164d0b32a658a5684090fd0e4b6da
Samuel Fernandez <sfernandez@moval.es>
2024-03-14 15:08:06 +0100

    [NEW] base_wua_cayc : Creation of module adds selected field on res_company with the code of the cayc instances

---
+++ base_wua_cayc/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# 2024 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Water Users Association: CAYC Management",
+    "summary": "CAYC management for any water users association",
+    "description": "Includes wua code on the company form view",
+    "version": '10.0.1.1.0',
+    "category": "Water Users Associations",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+    ],
+    "data": [
+        "views/res_company_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
