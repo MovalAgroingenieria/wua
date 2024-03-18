9fbb14f58348284a0b9e995961283472d213a34b
Eduardo Iniesta <einiesta@moval.es>
2024-03-18 21:38:22 +0100

    [BUG] base_wua_infrastructure_gravity_hierarchy: _update_condition method modified.

--- base_wua_infrastructure_gravity_hierarchy/models/wua_watering.py
+++ base_wua_infrastructure_gravity_hierarchy/models/wua_watering.py
@@ -19,15 +19,28 @@ class WuaWatering(models.Model):
         resp = [('id', '=', False)]
         if condition:
             resp = []
+            condition_irrigationditch_id = None
+            condition_main_irrigationditch_id = None
             for condition_item in (condition or []):
                 if len(condition_item) == 3:
                     item_01 = condition_item[0]
                     item_02 = condition_item[1]
                     item_03 = condition_item[2]
                     # Modified EIS (2024-03-18)
-                    # This is not necessary! (remove method?)
                     # if item_01 == 'irrigationditch_id':
                     #     item_01 = 'main_irrigationditch_id'
-                    resp_item = (item_01, item_02, item_03)
-                    resp.append(resp_item)
+                    # resp_item = (item_01, item_02, item_03)
+                    if item_01 == 'irrigationditch_id':
+                        condition_irrigationditch_id = \
+                            ('irrigationditch_id', item_02, item_03)
+                        condition_main_irrigationditch_id = \
+                            ('main_irrigationditch_id', item_02, item_03)
+                    else:
+                        resp_item = (item_01, item_02, item_03)
+                        resp.append(resp_item)
+            if (resp and condition_irrigationditch_id and
+               condition_main_irrigationditch_id):
+                resp.append('|')
+                resp.append(condition_irrigationditch_id)
+                resp.append(condition_main_irrigationditch_id)
         return resp
