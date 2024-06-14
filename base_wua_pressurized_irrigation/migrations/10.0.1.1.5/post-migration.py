9aa507aa8093df4338253e56e72d38870a40462c
Salvador Sanchez Aroca <ssanchez@moval.es>
2024-06-14 11:38:51 +0200

    [UPD] base_wua_pressurized_irrigation: Added try catchs on post migration script [10.0.1.1.5] to avoid erros on insertion of wua_waterconnection_irrigation_shift_rel records.

--- base_wua_pressurized_irrigation/migrations/10.0.1.1.5/post-migration.py
+++ base_wua_pressurized_irrigation/migrations/10.0.1.1.5/post-migration.py
@@ -8,21 +8,29 @@ from odoo import api, SUPERUSER_ID
 def migrate(cr, registry):
 
     env = api.Environment(cr, SUPERUSER_ID, {})
-    env.cr.execute("""
-        SELECT waterconnection_id, irrigation_shift_id
-        FROM wua_waterconnection_irrigation_shift_rel;
-    """)
-    records = env.cr.fetchall()
-
-    for record in records:
-        waterconnection_id, irrigation_shift_id = record
-        # Insert records directly into the database
-        cr.execute("""
-            INSERT INTO wua_waterconnection_irrigation_shift_relation
-            (waterconnection_id, irrigation_shift_id)
-            VALUES (%s, %s)
-        """, (waterconnection_id, irrigation_shift_id))
-
-    env.cr.execute("""
-        DROP TABLE IF EXISTS wua_waterconnection_irrigation_shift_rel;
-    """)
+    try:
+        env.cr.savepoint()
+        env.cr.execute("""
+            SELECT waterconnection_id, irrigation_shift_id
+            FROM wua_waterconnection_irrigation_shift_rel;
+        """)
+        records = env.cr.fetchall()
+        for record in records:
+            waterconnection_id, irrigation_shift_id = record
+            # Insert records directly into the database
+            cr.execute("""
+                INSERT INTO wua_waterconnection_irrigation_shift_relation
+                (waterconnection_id, irrigation_shift_id)
+                VALUES (%s, %s)
+            """, (waterconnection_id, irrigation_shift_id))
+        env.cr.commit()
+    except Exception:
+        env.cr.rollback()
+    try:
+        env.cr.savepoint()
+        env.cr.execute("""
+            DROP TABLE IF EXISTS wua_waterconnection_irrigation_shift_rel;
+        """)
+        env.cr.commit()
+    except Exception:
+        env.cr.rollback()
