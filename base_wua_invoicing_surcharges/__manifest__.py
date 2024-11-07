427b6d30cc071d70fc01e096d1807a40df9a4a1e
Salvador Sanchez Aroca <ssanchez@moval.es>
2024-11-07 12:09:00 +0100

    [UPD] base_wua_invoicing_surcharges: Bump version for recalculus of number of invoicing fields.

--- base_wua_invoicing_surcharges/__manifest__.py
+++ base_wua_invoicing_surcharges/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Water Users Association: Invoice Surcharge Invoicing",
     "summary": "In a water users association, unpayed invoice "
                "invoicing with fixed or variable surcharges",
-    "version": '10.0.1.1.1',
+    "version": '10.0.1.1.2',
     "category": "Water Users Associations",
     "website": "http://www.moval.es",
     "author": "Moval Agroingeniería",
