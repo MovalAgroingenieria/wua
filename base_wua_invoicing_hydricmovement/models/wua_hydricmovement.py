2b1357624719e0595db017059eedc9323ae8627f
Samuel Fernandez <sfernandez@moval.es>
2024-11-21 16:44:18 +0100

    [UPD] base_wua_invoicing_hydricmovement: Changed compute_number_of_invoices to count all registers on multicompany instances.

--- base_wua_invoicing_hydricmovement/models/wua_hydricmovement.py
+++ base_wua_invoicing_hydricmovement/models/wua_hydricmovement.py
@@ -31,6 +31,8 @@ class WuaHydricmovement(models.Model):
     def _compute_number_of_invoices(self):
         for record in self:
             number_of_invoices = 0
-            if record.invoiceline_ids:
-                number_of_invoices = len(record.invoiceline_ids)
+            invoiceline_ids = record.invoiceline_ids.with_context(
+                force_company=False)
+            if invoiceline_ids:
+                number_of_invoices = len(invoiceline_ids)
             record.number_of_invoices = number_of_invoices
