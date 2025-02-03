07afa15196b6ceee74f1df375a3556122e757ad7
Juanu Sandoval <jsandoval@moval.es>
2025-02-03 17:33:21 +0100

    [UPD] base_wua_infrastructure: Apply Flake8 rules to code

--- base_wua_infrastructure/wizard/waterconnection_mail_wizard.py
+++ base_wua_infrastructure/wizard/waterconnection_mail_wizard.py
@@ -4,32 +4,30 @@ from openerp import api, fields, models
 
 class WuaWaterconnectionMailWizard(models.TransientModel):
     _name = 'wua.waterconnection.mail.wizard'
-    
+
     shared_title = fields.Char(
         string='Shared Title',
-        help='The title that will be included in the email for all selected records.'
-    )
+        help='The title that will be included in the email for all selected'
+             'records.')
+
     shared_text = fields.Text(
         string='Shared Text',
-        help='Text that will be included in the email for all selected records.'
-    )
-    
+        help='Text that will be included in the email for all selected '
+             'records.')
+
     @api.multi
     def action_send_mails(self):
-        """
-        Envía el correo usando una plantilla, aplicando el shared_text a cada registro.
-        """
         self.ensure_one()
         active_ids = self._context.get('active_ids', [])
         WaterConnection = self.env['wua.waterconnection']
         waterconnections = WaterConnection.browse(active_ids)
-        
-        # Referencia a la plantilla de correo (definida en XML)
+
         template = self.env.ref(
-            'base_wua_infrastructure.email_template_waterconnection_notice', False)
+            'base_wua_infrastructure.email_template_waterconnection_notice',
+            False)
         if not template:
             return True
-        
+
         for wc in waterconnections:
             possible_partners = []
             for irrigationpoint in wc.irrigationpoint_ids:
@@ -43,7 +41,6 @@ class WuaWaterconnectionMailWizard(models.TransientModel):
                     partner_to_sent_id = possible_partners[0]
             partner_rec = self.env['res.partner'].browse(partner_to_sent_id)
             partner_email = partner_rec.email
-            # Agregamos el shared_text en el contexto para usarlo en la plantilla
             ctx = {
                 'default_model': 'wua.waterconnection',
                 'default_res_id': wc.id,
@@ -52,8 +49,6 @@ class WuaWaterconnectionMailWizard(models.TransientModel):
                 'partner_to_sent': partner_rec,
                 'partner_email': partner_email,
             }
-            # Enviar el correo individual y obtener el ID de mail.mail generado
             template.with_context(ctx).send_mail(wc.id, force_send=True)
 
-        
         return True
