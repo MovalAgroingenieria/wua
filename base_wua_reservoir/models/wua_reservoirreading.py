2f23da2f21801596160db3ab88dbe7ec8270fb33
Juanu Sandoval <jsandoval@moval.es>
2025-08-28 14:34:13 +0200

    [UPD] base_wua_reservoir: Change date based on '%x' to common function

--- base_wua_reservoir/models/wua_reservoirreading.py
+++ base_wua_reservoir/models/wua_reservoirreading.py
@@ -2,7 +2,6 @@
 # 2021 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import datetime
 import pytz
 from odoo import models, fields, api, exceptions, _
 
@@ -263,9 +262,10 @@ class WuaReservoirReading(models.Model):
                 reading_time_str = str(reading_time)
                 date_str = reading_time_str[:10]
                 hour_str = reading_time_str[-8:]
-                name = reservoir_name + ' - ' + \
-                    datetime.datetime.strptime(
-                        date_str, '%Y-%m-%d').strftime('%x') + ' ' + hour_str
+                date_str_localized = self.env['wua.parcel'].\
+                    transform_date_to_locale(date_str)
+                name = reservoir_name + ' - ' + date_str_localized + \
+                    ' ' + hour_str
                 result.append((record.id, name))
         return result
 
