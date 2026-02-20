46ec30f0a19f65ae07d9164c6a452b3cc7f00d3a
Juanu Sandoval <jsandoval@moval.es>
2026-02-20 12:08:39 +0100

    [FIX] account_wua: Repare method to calculate invoice_payments

--- account_wua/reports/account_invoice.py
+++ account_wua/reports/account_invoice.py
@@ -2,7 +2,7 @@
 # 2024 Moval Agroingeniería
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models, _
+from odoo import models
 
 
 class AccountInvoice(models.Model):
@@ -10,21 +10,25 @@ class AccountInvoice(models.Model):
 
     def _get_invoice_payments(self, invoice):
         payments = []
-        account_move_lines = invoice.move_id.line_ids
-        account_move_line_ids = []
-        for account_move_line in account_move_lines:
-            for aml in account_move_line:
-                if aml.account_id.reconcile:
-                    account_move_line_ids.extend(
-                        [r.debit_move_id.id for r in aml.matched_debit_ids]
-                        if aml.credit > 0 else
-                        [r.credit_move_id.id for r in aml.matched_credit_ids])
-                    account_move_line_ids.append(aml.id)
-        for account_move_line_id in account_move_line_ids:
-            reconciled_move_line = self.env['account.move.line'].browse(
-                account_move_line_id)
-            if reconciled_move_line.credit > 0:
-                payments.append(
-                    [reconciled_move_line.date,
-                     reconciled_move_line.credit])
+        receivable_lines = invoice.move_id.line_ids.filtered(
+            lambda l: l.account_id.internal_type in ('receivable', 'payable')
+        )
+
+        for line in receivable_lines:
+            # Payments customer invoice
+            for partial in line.matched_credit_ids:
+                payment_line = partial.credit_move_id
+                payments.append([
+                    payment_line.date,
+                    partial.amount
+                ])
+
+            # Payments vendor bill
+            for partial in line.matched_debit_ids:
+                payment_line = partial.debit_move_id
+                payments.append([
+                    payment_line.date,
+                    partial.amount
+                ])
+
         return payments
