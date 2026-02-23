50b87387342672a391976b24328e8155f75c1ade
Guillermo Amante <gamante@moval.es>
2026-02-23 09:27:31 +0100

    [UPD] base_wua_remotesensing_sentinelhub: add controller to manage ndvi timeseries

--- base_wua_remotesensing_sentinelhub/__init__.py
+++ base_wua_remotesensing_sentinelhub/__init__.py
@@ -3,5 +3,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import models
+from . import controllers
 from hooks import pre_init_hook
 from hooks import uninstall_hook
