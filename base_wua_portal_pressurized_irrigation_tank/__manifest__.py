1eea4363cbddb59511a75571bae72299f5b32a36
Samuel Fernandez <sfernandez@moval.es>
2026-02-26 16:51:02 +0100

    [NEW] base_wua_portal_pressurized_irrigation_tank: Add new modules related to tankconsumptions and batchline integration with tanks.

---
+++ base_wua_portal_pressurized_irrigation_tank/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# 2025 Moval Agroingeniería
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name":
+    "Water Users Association: Pressurized Irrigation Tank Portal user experience",
+    "summary": "Module that improves the user experience of the portal",
+    "version": '10.0.0.0.0',
+    "category": "Water Users Associations",
+    "website": "http://www.moval.es",
+    "author": "Moval Agroingeniería",
+    "license": "AGPL-3",
+    "depends": [
+        "base_wua_portal_pressurized_irrigation",
+        "base_wua_pressurized_irrigation_tank",
+    ],
+    "data": [
+        "views/website_portal_templates.xml",
+        "views/portal_irrigationmanagement_templates.xml",
+        'security/ir.model.access.csv',
+    ],
+    "installable": True,
+    "application": False,
+}
